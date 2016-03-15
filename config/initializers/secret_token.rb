b7d1ab5dade3f56937fc841701ac9824be3a52ea
Your Name <you@example.com>
2016-03-15 07:48:39 -0400

    kd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gstp::Application.config.secret_key_base = '5b4add7bfe7190284e1af5e10556348572effb166c8e73c24e180c9c542b326ad650f0bd817b66ccfbaa53376999b1aa11ef764d9c873167e92077dc6fca931d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gstp::Application.config.session_store :cookie_store, key: '_gstp_session'
